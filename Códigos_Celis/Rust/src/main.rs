115f6305197d9a6a272e66dd286917cf748a0256
Juan_Celis <juanccelislozada13@gmail.com>
2026-03-22 20:47:40 -0500

    Autovalores y autovectores en rust.

--- Códigos_Celis/Rust/src/main.rs
+++ Códigos_Celis/Rust/src/main.rs
@@ -1,30 +1,32 @@
 mod gauss;
 mod descomposicion_lu;
+mod autovalores_autovectores;
+
 use ndarray::{array, Array1, Array2, s};
 use std::time::{Duration, Instant};
+use crate::autovalores_autovectores::{autovectores_autovalores, matrices_qr};
+
 fn main() {
+
     let A = array![
-        [4.0,-2.0,1.0],
-        [20.0,-7.0,12.0],
-        [-8.0,13.0,17.0]
+        [1.0, 4.0, 8.0, 4.0],
+        [4.0, 2.0, 3.0, 7.0],
+        [8.0, 3.0, 6.0, 9.0],
+        [4.0, 7.0, 9.0, 2.0]
     ];
 
-    let (L_matrices, U, L_inv) = descomposicion_lu::descomposicion_lu(A.clone());
+    let (Q, R) = matrices_qr(&A);
 
-    for (i, L) in L_matrices.iter().enumerate() {
-        println!("L {}", i);
-        println!("{:?}", L);
-    }
+    println!("Matriz Q:\n{:?}", Q);
+    println!("Matriz R:\n{:?}", R);
 
-    println!("\nMatriz U:");
-    println!("{:?}", U);
+    let n = Q.dot(&R);
+    println!("Q * R:\n{:?}", n);
 
-    println!("\nMatriz L inv:");
-    println!("{:?}", L_inv);
+    let (autovalores, V, A_final) = autovectores_autovalores(A);
 
-    let A_rec = L_inv.dot(&U);
-
-    println!("\n{:?}", A_rec);
-
+    println!("Autovalores:\n{:?}", autovalores);
+    println!("Matriz A final:\n{:?}", A_final);
+    println!("Autovectores (V):\n{:?}", V);
 }
 
